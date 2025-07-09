156587ac4e72aa53fd7bc7fe6feed82c74b12dc6
Oluwapelumi <oadegboyega@academic.rrc.ca>
2025-07-09 13:06:32 -0500

    Part 1: Created customer partial with styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  class CustomersController < ApplicationController
+    def index
+      @customers = Customer.all
+    end
+
+    def alphabetized
+      @customers = Customer.order(:full_name)
+      render :index
+    end
+
+    def missing_email
+      @customers = Customer.where(email_address: [nil, ''])
+      render :index
+    end
+  end
   allow_browser versions: :modern
 end
+
+
